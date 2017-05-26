b4cb912353c7dad88d97f05112b8ccfa80a1a6c4
Darron Brown <DarronBrown88@outlook.com>
2017-05-26 13:14:04 -0600

    NewsFeedContainer.js added

--- src/containers/NewsFeedContainer.js
+++ src/containers/NewsFeedContainer.js
@@ -2,10 +2,11 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { loadNews } from '../actions/newsActions';
 import NewsFeed from '../components/NewsFeed';
-import { reshapeNewsData } from '../util/dataTransformation';
+import { reshapeNewsData } from '../util/dataTransformations';
+import { allNewsSelectors } from '../selectors/newsSelectors';
 
 const mapStateToProps = state => ({
-  news: reshapeNewsData(state.news)
+  news: allNewsSelectors(state)
 });
 
 const mapDispatchToProps = dispatch => (
